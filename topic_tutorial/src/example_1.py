6a041546c2ccce3c9a3a9967e4799a845bed9bf5
kimsooyoung <tge1375@naver.com>
2021-01-05 15:24:28 +0900

    Feature : DriveForward example

---
+++ topic_tutorial/src/example_1.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import time
+import rospy
+from geometry_msgs.msg import Twist
+
+rospy.init_node('DriveForward')
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1 )
+r = rospy.Rate(1)
+
+forward = Twist()
+stop = Twist()
+
+forward.linear.x = 0.5
+stop.linear.x = 0.0
+
+start_time = time.time()
+
+rospy.loginfo("==== DriveForward node Started, move forward during 10 seconds ====\n")
+
+
+while not rospy.is_shutdown():
+    if (time.time() - start_time < 10.0):
+        pub.publish(forward)
+    else:
+        rospy.logwarn(" 10 seconds left, Stop!! ")
+        pub.publish(stop)
+        break
+
