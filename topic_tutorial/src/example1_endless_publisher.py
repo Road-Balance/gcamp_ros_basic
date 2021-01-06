652644bb6ec7bd27aaf8963ab2a0db8204195a44
kimsooyoung <tge1375@naver.com>
2021-01-06 11:59:06 +0900

    Update : Endless Publisher / Feature : Service package added

---
+++ topic_tutorial/src/example1_endless_publisher.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import time
+import rospy
+from geometry_msgs.msg import Twist
+
+rospy.init_node('drive_forward')
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1 )
+r = rospy.Rate(1)
+
+forward = Twist()
+stop = Twist()
+
+forward.linear.x = 0.5
+stop.linear.x = 0.0
+
+start_time = time.time()
+
+rospy.loginfo("==== Endless DriveForward node Started ====\n")
+
+
+while not rospy.is_shutdown():
+    pub.publish(forward)
+
+
