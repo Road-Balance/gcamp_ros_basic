3dceb28e5a7c98e9b17342cb38a2c244b707b845
kimsooyoung <tge1375@naver.com>
2021-01-07 15:55:22 +0900

    Subscriber example updated

--- topic_tutorial/src/example2_subscriber.py
+++ topic_tutorial/src/example2_subscriber.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import LaserScan
 case = 'spin_mode'
 
 def callback(data):
+    # data.ranges
     print(data)
 
 rospy.loginfo("==== Laser Scan Subscriber node Started, move forward during 10 seconds ====\n")
