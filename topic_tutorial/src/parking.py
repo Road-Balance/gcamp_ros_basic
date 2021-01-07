36b0e243d86781ed59600a94aad3373944a7073c
kimsooyoung <tge1375@naver.com>
2021-01-07 15:54:20 +0900

    Feature: Parking example added

---
+++ topic_tutorial/src/parking.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+def callback(data):
+    laser_range = data.ranges
+    cmd_vel = Twist()
+
+    if laser_range[360] > 0.8:
+        cmd_vel.linear.x = 1.0
+    else:
+        cmd_vel.linear.x = 0.0
+        pub.publish(cmd_vel)
+
+        sub.unregister()
+        rospy.signal_shutdown("Parking Done")
+
+    pub.publish(cmd_vel)
+
+
+rospy.init_node("parking", disable_signals=True)
+rospy.loginfo("==== parking node Started ====\n")
+
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+sub = rospy.Subscriber("/scan", LaserScan, callback)
+
+rospy.spin()
+
