70c1db1869f3fae33dc43dcee0b66fe45ff0d6df
kimsooyoung <tge1375@naver.com>
2021-01-14 02:20:49 +0900

    Feature : parking example added

--- cpp_topic_tutorial/src/parking.cpp
+++ cpp_topic_tutorial/src/parking.cpp
@@ -2,37 +2,47 @@
 #include <iostream>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
+class TinyBot
+{
+private:
+    ros::NodeHandle m_nh;
+    ros::Publisher m_pub;
+    ros::Subscriber m_sub;
 
-static geometry_msgs::Twist go_forward;
-static geometry_msgs::Twist stop;
-go_forward.linear.x = 0.3;
-stop.linear.x = 0.3;
+    geometry_msgs::Twist m_cmd_vel;
 
-void sub_callback(const sensor_msgs::LaserScan &data){
-    auto frontLidarPoint = data.ranges[360];
-
-    if ( frontLidarPoint >= 0.3 ){
-        pub.publish(go_forward);
+public:
+    TinyBot(){
+        m_pub = m_nh.advertise<geometry_msgs::Twist>("/cmd_vel", 5);
+        m_sub = m_nh.subscribe("/scan", 1, &TinyBot::subCallback, this);
     }
-    else {
-        pub.publish(stop);
+
+    void robotStop(){
+        m_cmd_vel.linear.x = 0.0;
+        m_pub.publish(m_cmd_vel);
     }
 
-    ROS_INFO("===== front lidar point val : %d", frontLidarPoint);
-}
+    void robotGo(){
+        m_cmd_vel.linear.x = 0.3;
+        m_pub.publish(m_cmd_vel);
+    }
+
+    void subCallback(const sensor_msgs::LaserScan &data){
+        auto frontLidarPoint = data.ranges[360];
+
+        if ( frontLidarPoint >= 0.3 ){
+            this->robotGo();
+        }
+        else {
+            this->robotStop();
+        }
+
+        ROS_INFO("===== front lidar point val : %f", frontLidarPoint);
+    }
+};
 
 int main(int argv, char** argc) {
 
-    ros::init(argv, argc, "parking_node");
-    ros::NodeHandle nh;
-    
-    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 5);
-    ros::Subscriber sub = nh.subscribe("/scan", 1, sub_callback);
-
-    
-    ros::Rate r(5);
-    
-    ros::spin();
 
     return 0;
 }
