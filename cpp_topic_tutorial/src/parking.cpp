0f836f372afb1b27eb4e032b9cb15c97d64624a6
kimsooyoung <tge1375@naver.com>
2021-01-14 02:34:47 +0900

    Update: parking example done

--- cpp_topic_tutorial/src/parking.cpp
+++ cpp_topic_tutorial/src/parking.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
+
 class TinyBot
 {
 private:
@@ -13,6 +14,7 @@ private:
 
 public:
     TinyBot(){
+        ROS_INFO("Publisher and Subscriber initialized");
         m_pub = m_nh.advertise<geometry_msgs::Twist>("/cmd_vel", 5);
         m_sub = m_nh.subscribe("/scan", 1, &TinyBot::subCallback, this);
     }
@@ -23,14 +25,16 @@ public:
     }
 
     void robotGo(){
-        m_cmd_vel.linear.x = 0.3;
+        m_cmd_vel.linear.x = 0.8;
         m_pub.publish(m_cmd_vel);
     }
 
     void subCallback(const sensor_msgs::LaserScan &data){
-        auto frontLidarPoint = data.ranges[360];
 
-        if ( frontLidarPoint >= 0.3 ){
+        auto frontLidarPoint = data.ranges[360];
+        std::cout << frontLidarPoint << std::endl;
+
+        if ( frontLidarPoint >= 1.0 ){
             this->robotGo();
         }
         else {
@@ -43,6 +47,9 @@ public:
 
 int main(int argv, char** argc) {
 
+    ros::init(argv, argc, "parking_node");
+    TinyBot tinybot;
+    ros::spin();
 
     return 0;
 }
